e9f99c41841fb8ecb1fe3bd25d0558c8ee3b539d
Alex Staroselsky <AlStar01@users.noreply.github.com>
2017-08-25 09:12:16 -0600

    Added basic logic for nodemailer gmail smtp transport.

--- SendContactEmail/index.js
+++ SendContactEmail/index.js
@@ -1,3 +1,9 @@
+const nodemailer = require('nodemailer');
+
+const gmailEmail = GetEnvironmentVariable("GMAIL_EMAIL");
+const gmailPassword = GetEnvironmentVariable("GMAIL_PASSWORD");
+const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -6,8 +12,7 @@ module.exports = function (context, req) {
             // status: 200, /* Defaults to 200 */
             body: "Hello " + (req.query.name || req.body.name)
         };
-    }
-    else {
+    } else {
         context.res = {
             status: 400,
             body: "Please pass a name on the query string or in the request body"
@@ -15,3 +20,38 @@ module.exports = function (context, req) {
     }
     context.done();
 };
+
+function sendWelcomeEmail(contact) {
+    const { name, email, subject, message } = contact;
+
+    const mailOptions = {
+        from: gmailEmail,
+        to: gmailEmail,
+        replyTo: `${name} <${email}>`,
+        subject: subject,
+        text: `Contact Request - ${name} <${email}> - ${message}`,
+        html: `
+            Contact Request
+
+            <div><strong>Name:</strong> ${name}</div>
+            <br />
+            <div><strong>Name:</strong> ${email}</div>
+            <br />
+            <div><strong>Name:</strong> ${subject}</div>
+            <br />
+            <div><strong>Name:</strong> ${message}</div>
+
+            <br />
+            <br />
+
+            Thanks!
+        `
+    };
+
+    return mailTransport.sendMail(mailOptions);
+
+}
+
+function GetEnvironmentVariable(name) {
+    return process.env[name];
+}
